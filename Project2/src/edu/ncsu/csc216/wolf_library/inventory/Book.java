823645d1a8cd06234e3769b8ddfd0386bac68b74
mflores <mflores@ncsu.edu>
2015-06-18 18:54:55 -0400

    Initial commit of project skeleton.

---
+++ Project2/src/edu/ncsu/csc216/wolf_library/inventory/Book.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package edu.ncsu.csc216.wolf_library.inventory;
+
+/**
+ * This class defines the methods of a book object for the Wolf Library project. It contains
+ * the information about the book as well as methods for accessing the book and keeping track
+ * of the copies currently in stock.
+ * @author Marcus
+ *
+ */
+public class Book {
+
+    /**
+     * 
+     */
+    public Book() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
