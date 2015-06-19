05190d964a8ea63f01c31f50caecd7f77a56cea9
mflores <mflores@ncsu.edu>
2015-06-18 20:53:26 -0400

    Started book database.

---
+++ Project2/src/edu/ncsu/csc216/wolf_library/inventory/BookDB.java
@@ -0,0 +1,25 @@
+package edu.ncsu.csc216.wolf_library.inventory;
+
+
+/**
+ * The book db class represents an internal book data base used by the Wolf Library system. It consists
+ * of a multi-purpose list used to keep track of the books in the library.
+ * @author Marcus
+ *
+ */
+public class BookDB {
+    /**
+     * The books data member is an array that holds the actual books from a file.
+     */
+    private Book [] books;
+    /**
+     * 
+     */
+    
+    
+
+    public BookDB() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
